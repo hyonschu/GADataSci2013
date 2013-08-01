6703c75d0d377da8156e3a274e5f603505e9818d
hyonschu <eatkimchi@outlook.com>
2013-07-31 22:36:16 -0700

    Update script.R

--- Assign3/script.R
+++ Assign3/script.R
@@ -1,27 +1,3 @@
-
-R version 3.0.1 (2013-05-16) -- "Good Sport"
-Copyright (C) 2013 The R Foundation for Statistical Computing
-Platform: x86_64-apple-darwin10.8.0 (64-bit)
-
-R is free software and comes with ABSOLUTELY NO WARRANTY.
-You are welcome to redistribute it under certain conditions.
-Type 'license()' or 'licence()' for distribution details.
-
-  Natural language support but running in an English locale
-
-R is a collaborative project with many contributors.
-Type 'contributors()' for more information and
-'citation()' on how to cite R or R packages in publications.
-
-Type 'demo()' for some demos, 'help()' for on-line help, or
-'help.start()' for an HTML browser interface to help.
-Type 'q()' to quit R.
-
-[R.app GUI 1.61 (6492) x86_64-apple-darwin10.8.0]
-
-[Workspace restored from /Users/hyonschu/.RData]
-[History restored from /Users/hyonschu/.Rapp.history]
-
 > library("e1071")
 Loading required package: class
 > data(iris)
